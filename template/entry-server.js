404f818f0e8df18dda70b4ef10dbe9469d72418c
ysk2014 <1181102772@qq.com>
2018-05-12 18:37:00 +0800

    change

--- template/entry-server.js
+++ template/entry-server.js
@@ -2,57 +2,6 @@ import root from '@/app'
 import {stringify} from "querystring"
 import { urlJoin, createRedirect, promisify } from "./utils"
 
-<<<<<<< HEAD
-const isDev = process.env.NODE_ENV !== 'production'
-
-export default context => {
-  return new Promise((resolve, reject) => {
-
-    if (!root.data || typeof root.data != "function") {
-      return reject(new Error("The app.js file must have a data method, and the data must be a function"))
-    }
-
-    const { app, router, store } = root.data(context);
-
-    const { req } = context
-    const { fullPath } = router.resolve(req.url).route
-
-    if (fullPath !== req.url) {
-      return reject({ url: fullPath })
-    }
-
-    // set router's location
-    router.push(req.url)
-
-    // wait until router has resolved possible async hooks
-    router.onReady(() => {
-      const matchedComponents = router.getMatchedComponents()
-      // no matched routes
-      if (!matchedComponents.length) {
-        return reject({ code: 404 })
-      }
-      // Call fetchData hooks on components matched by the route.
-      // A preFetch hook dispatches a store action and returns a Promise,
-      // which is resolved when the action is complete and store state has been
-      // updated.
-
-      Promise.all(matchedComponents.map(({ asyncData }) => asyncData && asyncData({
-        store,
-        route: router.currentRoute,
-        context
-      }))).then(() => {
-        // After all preFetch hooks are resolved, our store is now
-        // filled with the state needed to render the app.
-        // Expose the state on the render context, and let the request handler
-        // inline the state in the HTML response. This allows the client-side
-        // store to pick-up the server-side state without having to duplicate
-        // the initial data fetching on the client.
-        context.state = store.state
-        resolve(app)
-      }).catch(reject)
-    }, reject)
-  })
-=======
 const isDev = process.env.NODE_ENV !== 'production';
 
 const noopApp = () => new Vue({ render: (h) => h('div') })
@@ -131,5 +80,4 @@ export default (context) => {
 
 		}, reject)
 	})
->>>>>>> 61144edede521b62cd3f0f17456b5dcbfae69300
 }
