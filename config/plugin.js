09ed02a69264a7d2327762d1a555a60fcf3530a0
ysk2014 <1181102772@qq.com>
2018-10-13 12:40:28 +0800

    change

--- config/plugin.js
+++ config/plugin.js
@@ -35,26 +35,6 @@ exports.html = {
     }
 };
 
-exports.vendor = {
-    args: {
-        name: "vendor",
-        minChunks: function(module) {
-            // any required modules inside node_modules are extracted to vendor
-            return (
-                module.resource &&
-                /\.js$/.test(module.resource) &&
-                module.resource.indexOf(
-                    path.join(process.cwd(), "./node_modules")
-                ) === 0 &&
-                !/node_modules[\/\\]{1}flow\-vue\-ssr\-hook[\/\\]{1}template[\/\\]{1}/.test(
-                    module.resource
-                ) &&
-                !/\.(css|less|scss|sass|styl|stylus|vue)$/.test(module.request)
-            );
-        }
-    }
-};
-
 exports.vue = {
     enable: true,
     name: "vue-loader/lib/plugin"
